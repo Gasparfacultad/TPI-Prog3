fd97c73bb811cd488e6b9ab1e40f6ef447ea651f
GasparGIT <gasparuniversidad@gmail.com>
2025-11-17 11:30:48 -0300

    feat(endpoints) : agregar archivo con las URLs de la API

---
+++ JS/endpoints.js
@@ -0,0 +1,3 @@
+const URL_USERS = "https://6914d9f23746c71fe049d5a8.mockapi.io/api/v1/Users";
+const URL_MENU = "https://6914e41e3746c71fe049f536.mockapi.io/api/v2/menu";
+const URL_ORDERS = "https://6914e41e3746c71fe049f536.mockapi.io/api/v2/orders";
